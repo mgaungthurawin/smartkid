df1e6c4e4b3b821d1e0330e3fa59c7f916e6f161
Aung Thura <aungthurawin@mzonlinemarket.com>
2021-07-15 11:14:13 +0630

    update faq

--- routes/web.php
+++ routes/web.php
@@ -15,31 +15,24 @@ Route::get('test', function () {
 	return config('story');
 });
 Route::get('/', 'WebController@landing');
+Route::get('/faq', 'WebController@faq');
 Route::group(['middleware' => ['cors', 'signature']], function () {
 	Route::get('/categories', 'WebController@categories');
 	Route::get('/horoscope', 'WebController@horoscope');
 	Route::get('/horoscope/{type}', 'WebController@horoscopeType');
 	Route::get('/myprofile', 'WebController@profile');
-	Route::get('/faq', 'WebController@faq');
 	Route::get('/songforkid', 'WebController@songforkid');
 	Route::get('/profile', 'WebController@profile');
 	Route::post('profile', 'WebController@postProfile');
 	Route::get('games', 'WebController@games');
 	Route::get('braintester', 'WebController@braintester');
-
 	Route::get('/health', 'WebController@health');
 	Route::get('/health/{id}/detail', 'WebController@healthDetail');
-
 	Route::get('/storyforkid', 'WebController@storyforkid');
 	Route::get('/story/{id}/detail', 'WebController@storyforkidDetial');
-
 	Route::get('/educationvideo', 'WebController@educationvideo');
 	Route::get('/education/{id}/detail', 'WebController@educationvideoDetail');
-
 	Route::get('post/{id}/{type}/favourite', 'WebController@postFavourite');
-
 	Route::get('unsubscribe', 'WebController@unsubscribe');
-
-
 });
 
